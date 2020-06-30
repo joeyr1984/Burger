c6c7c04d4ab419499f49eeb4da50467092b48adf
Joey Reyes <joeyr1984@gmail.com>
2020-06-29 17:53:23 -0700

    added to schema files

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Impossible Whopper');
+INSERT INTO burgers (name) VALUES ('Chilli Burger');
+INSERT INTO burgers (name, devoured) VALUES ('Veggie Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Quarter Pounder Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Double Double Burger', true);
+INSERT INTO burgers (name) VALUES ('Mushroom and Swiss Burger');
