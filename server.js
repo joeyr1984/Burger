d69b6a077cda7d25e017063d49254cf83de87fcb
Joey Reyes <joeyr1984@gmail.com>
2020-06-30 19:31:05 -0700

    adding syntax to orm, burgerjs and added styling and syntax to handlebars

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+
+app.use(express.static("public"));
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+const routes = require("");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("App now listening at localhost:" + PORT);
+});
